e72b53891c40aa791d369f15351307a6598cc501
Dieter De Witte <drdwitte@gmail.com>
2016-03-09 17:36:25 +0100

    removed mapquest developer logo since it slows down page load

--- js/app/helper.js
+++ js/app/helper.js
@@ -248,7 +248,7 @@ function renameKeys(dict, newKeys){
 
 function createBaseLayer(){
 
-	var attributionString = '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">';
+	var attributionString = '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a>';
 	var subdomainsToQuery = ['otile1','otile2','otile3','otile4'];
 	var s1 = 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png'; //map
 	//var s2 = 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png'; //satellite
